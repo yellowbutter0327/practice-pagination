35ec9eaddee0dc54545cfad3f230ce78a953cc00
joeunhye <slrkwhdmsp@naver.com>
2023-07-14 18:13:51 +0900

    FEAT:페이지네이션 연습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Posts from "./pages/Main/Main";
+import Pagination from "./pages/Pagination/Pagination";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      setPosts(response.data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  /* 새로 추가한 부분 */
+  const indexOfLast = currentPage * postsPerPage;
+  const indexOfFirst = indexOfLast - postsPerPage;
+  const currentPosts = (posts) => {
+    let currentPosts = 0;
+    currentPosts = posts.slice(indexOfFirst, indexOfLast);
+    return currentPosts;
+  };
+  /*                 */
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Posts posts={currentPosts(posts)} loading={loading}></Posts>
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={setCurrentPage}
+      ></Pagination>
     </div>
   );
 }
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Main.scss";
+
+const Main = () => {
+  return (
+    <div className="container">
+      <div>배고프다</div>
+    </div>
+  );
+};
+
+export default Main;
